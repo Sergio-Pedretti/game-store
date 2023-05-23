823b7a4f7d60aa33e07b0db2747d5af0fc055131
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-05-23 14:51:34 -0300

    chore: add husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const test = 'aaaa'
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
