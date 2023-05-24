213faec1311befb0c0ef5063b9e59a1eb0f06f08
Sergio-Pedretti <sergiopedretti07@gmail.com>
2023-05-24 15:20:22 -0300

    feat: start main component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
+  <main className="container h-screen">
+    <h1 className="text-lg font-bold text-gray-900">React Avançado</h1>
   </main>
 )
